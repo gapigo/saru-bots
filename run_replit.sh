90f0f974f8d3cd9d119a87e1b87c724fd3645f76
gapigo <x.gabriel12@gmail.com>
2022-05-13 13:28:48 -0300

    Adding replit build

---
+++ run_replit.sh
@@ -0,0 +1,16 @@
+python3 -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+pip install discord
+
+python_location=$(python -c "import sys; print(sys.executable)")
+parentdir1="$(dirname "$python_location")"
+parentdir2="$(dirname "$parentdir1")"
+array=($(ls -d $parentdir2/lib/))
+script_file="$array[-1]/site-packages/flask_script/__init__.py"
+flask_script="$parentdir2$script_file"
+mv $flask_script "$flask_script".old
+cp flask_app/flask_script__init__.py $flask_script
+
+python run.py
